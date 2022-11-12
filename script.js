78185464d172c686603320285d579aa855da7f6c
Mert Eldemir <113149328+merteldem1r@users.noreply.github.com>
2022-11-13 02:34:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,99 +11,97 @@ const stop = document.getElementById("stop");
 const newResult = document.getElementById("new");
 const clearResults = document.getElementById("clearResults");
 
+const checkValue = (value) => (value < 10 ? "0" + value : value);
+
 let timerInterval;
 
 // timer
 const timer = () => {
-    let mil = Number(milliseconds.innerHTML);
-    let sec = Number(seconds.innerHTML);
-    let min = Number(minutes.innerHTML);
-    let hour = Number(hours.innerHTML);
+  let mil = Number(milliseconds.innerHTML);
+  let sec = Number(seconds.innerHTML);
+  let min = Number(minutes.innerHTML);
+  let hour = Number(hours.innerHTML);
 
-    mil += 1;
-    if (mil < 10) milliseconds.innerHTML = `0${mil}`;
-    else milliseconds.innerHTML = mil;
+  mil += 1;
+  milliseconds.innerHTML = checkValue(mil);
 
-    // check milliseconds
-    if (mil == 100) {
-        mil = 0;
-        milliseconds.innerHTML = "00";
+  // check milliseconds
+  if (mil == 100) {
+    mil = 0;
+    milliseconds.innerHTML = "00";
 
-        // seconds
-        sec += 1;
-        if (sec < 10) seconds.innerHTML = `0${sec}`
-        else seconds.innerHTML = sec;
-    }
+    // seconds
+    sec += 1;
+    seconds.innerHTML = checkValue(sec);
+  }
 
-    // check seconds
-    if (sec == 60) {
-        sec = 0;
-        seconds.innerHTML = "00";
+  // check seconds
+  if (sec == 60) {
+    sec = 0;
+    seconds.innerHTML = "00";
 
-        // minutes
-        min += 1;
-        if (min < 10) minutes.innerHTML = `0${min}`
-        else minutes.innerHTML = min;
-    }
+    // minutes
+    min += 1;
+    minutes.innerHTML = checkValue(min);
+  }
 
-    // check minutes
-    if (min == 60) {
-        min = 0;
-        minutes.innerHTML = "00";
+  // check minutes
+  if (min == 60) {
+    min = 0;
+    minutes.innerHTML = "00";
 
-        // hours
-        hour += 1;
-        if (hour < 10) hours.innerHTML = `0${hour}`
-        else hours.innerHTML = hour;
-    }
-}
+    // hours
+    hour += 1;
+    hours.innerHTML = checkValue(hour);
+  }
+};
 
 // start timer
-start.addEventListener('click', () => {
-    if (!timerInterval) timerInterval = setInterval(timer, 10);
+start.addEventListener("click", () => {
+  if (!timerInterval) timerInterval = setInterval(timer, 10);
 });
 
 // pause timer
-pause.addEventListener('click', () => {
-    clearInterval(timerInterval);
-    timerInterval = 0;
+pause.addEventListener("click", () => {
+  clearInterval(timerInterval);
+  timerInterval = 0;
 });
 
-// stop timer 
-stop.addEventListener('click', () => {
-    clearInterval(timerInterval);
-    timerInterval = 0;
+// stop timer
+stop.addEventListener("click", () => {
+  clearInterval(timerInterval);
+  timerInterval = 0;
 
-    // clear timer ui
-    timerItems.forEach(item => {
-        item.innerHTML = "00";
-    });
+  // clear timer ui
+  timerItems.forEach((item) => {
+    item.innerHTML = "00";
+  });
 });
 
-// show new result 
+// show new result
 let prevTime;
 
-newResult.addEventListener('click', () => {
-    const resultsCount = notifBox.children.length;
-    const resultTime = `${hours.textContent}:${minutes.textContent}:${seconds.textContent}:${milliseconds.textContent}`;
-    if (resultTime == prevTime) return;
+newResult.addEventListener("click", () => {
+  const resultsCount = notifBox.children.length;
+  const resultTime = `${hours.textContent}:${minutes.textContent}:${seconds.textContent}:${milliseconds.textContent}`;
+  if (resultTime == prevTime) return;
 
-    let resultHtml = `
+  let resultHtml = `
             <div class="result">
                 <span>Result #${resultsCount + 1}</span> : ${resultTime}
             </div>
     `;
 
-    notifBox.insertAdjacentHTML('afterbegin', resultHtml);
-    notifBox.classList.add("active");
-    prevTime = resultTime;
+  notifBox.insertAdjacentHTML("afterbegin", resultHtml);
+  notifBox.classList.add("active");
+  prevTime = resultTime;
 });
 
 // clear results
-clearResults.addEventListener('click', () => {
-    notifBox.classList.remove("active");
-    setTimeout(() => {
-        notifBox.innerHTML = "";
-    }, 100)
-    prevTime = 0;
+clearResults.addEventListener("click", () => {
+  notifBox.classList.remove("active");
+  setTimeout(() => {
+    notifBox.innerHTML = "";
+  }, 100);
+  prevTime = 0;
 });
